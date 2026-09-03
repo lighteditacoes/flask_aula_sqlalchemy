887d9c47bd713372c9c9504a6bd8dc6cd412191e
aluno <aluno@DESKTOP-LABINFO.com>
2026-09-03 16:39:48 -0300

    implementado blueprint de livros

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
+from routes.livros import livros_bp
 from flask_migrate import Migrate
 from db import db
 from model import Usuario, Livro
@@ -7,62 +8,65 @@ from model import Usuario, Livro
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estante.db'
 db.init_app(app)
+
+app.register_blueprint(livros_bp)
+
 migrate=Migrate(app, db)
 
 
-@app.route('/')
-def home():
-    try:
-        livros = Livro.query.all()
-        return render_template("index.html", livros=livros)
-    except Exception as e:
-            return f"Erro:{e}"
+# @app.route('/')
+# def home():
+#     try:
+#         livros = Livro.query.all()
+#         return render_template("index.html", livros=livros)
+#     except Exception as e:
+#             return f"Erro:{e}"
     
-@app.route('/inserir', methods=["GET", "POST"])
-def inserir():
-    try:
-        if request.method == "POST":
-            titulo = request.form["titulo"]
-            autor = request.form["autor"]
-            ano = request.form["ano"]
-            genero = request.form["genero"]
-            livro = Livro(titulo=titulo, autor=autor, ano=ano, genero=genero)
-            #livro = Livro(titulo=request.form["titulo"], autor=request.form["autor"], ano=request.form["ano"], genero=request.form["genero"])
+# @app.route('/inserir', methods=["GET", "POST"])
+# def inserir():
+#     try:
+#         if request.method == "POST":
+#             titulo = request.form["titulo"]
+#             autor = request.form["autor"]
+#             ano = request.form["ano"]
+#             genero = request.form["genero"]
+#             livro = Livro(titulo=titulo, autor=autor, ano=ano, genero=genero)
+#             #livro = Livro(titulo=request.form["titulo"], autor=request.form["autor"], ano=request.form["ano"], genero=request.form["genero"])
 
-            db.session.add(livro)
-            db.session.commit()
-            return redirect(url_for("home"))
-        return "Livros inseridos com sucesso"
-    except Exception as e:
-        return f"Erro:{e}"
+#             db.session.add(livro)
+#             db.session.commit()
+#             return redirect(url_for("home"))
+#         return "Livros inseridos com sucesso"
+#     except Exception as e:
+#         return f"Erro:{e}"
 
-@app.route('/deletar/<int:id>', methods=["GET", "POST"])
-def deletar(id):
-    try:
-        if request.method == "POST":
-            livro = db.session.get(Livro, id)
-            db.session.delete(livro)
-            db.session.commit()
+# @app.route('/deletar/<int:id>', methods=["GET", "POST"])
+# def deletar(id):
+#     try:
+#         if request.method == "POST":
+#             livro = db.session.get(Livro, id)
+#             db.session.delete(livro)
+#             db.session.commit()
 
-            return redirect(url_for('home'))
-        return render_template("index.html")
-    except Exception as e:
-        return f"Erro:{e}"
+#             return redirect(url_for('home'))
+#         return render_template("index.html")
+#     except Exception as e:
+#         return f"Erro:{e}"
 
-@app.route('/editar/<int:id>', methods=["GET", "POST"])
-def editar(id):
-    try:
-        livro = Livro.query.get(id)
-        if request.method == "POST":
-            livro.titulo = request.form["titulo"] # ou request.form.get('titulo')
-            livro.autor = request.form["autor"] # ou request.form.get('autor')
-            livro.ano = request.form["ano"] # ou request.form.get('ano')
-            livro.genero = request.form["genero"] # ou request.form.get('genero')
-            db.session.commit()
-            return redirect(url_for('home'))
-        return render_template("editar.html", id_livro=id, livro=livro)
-    except Exception as e:
-            return f"Erro:{e}"
+# @app.route('/editar/<int:id>', methods=["GET", "POST"])
+# def editar(id):
+#     try:
+#         livro = Livro.query.get(id)
+#         if request.method == "POST":
+#             livro.titulo = request.form["titulo"] # ou request.form.get('titulo')
+#             livro.autor = request.form["autor"] # ou request.form.get('autor')
+#             livro.ano = request.form["ano"] # ou request.form.get('ano')
+#             livro.genero = request.form["genero"] # ou request.form.get('genero')
+#             db.session.commit()
+#             return redirect(url_for('home'))
+#         return render_template("editar.html", id_livro=id, livro=livro)
+#     except Exception as e:
+#             return f"Erro:{e}"
 
 if __name__ == '__main__':
     with app.app_context():
